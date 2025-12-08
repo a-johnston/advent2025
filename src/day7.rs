14233a03217e41018c1683d24e186981389be949
Adam Johnston <adam.johnston00@gmail.com>
2025-12-07 16:33:01 -0800

    fix bug, improve overall feel

--- src/day7.rs
+++ src/day7.rs
@@ -1,45 +1,62 @@
+use std::iter;
+
 use super::{common_parts, types::Part};
 
 pub static PARTS: &'static [Part<'static>] =
     &common_parts![count_classical_splits, count_quantum_splits];
 
-fn get_row_activations(s: &str) -> Vec<u64> {
-    s.chars().map(|c| (c != '.') as u64).collect()
+struct Row {
+    split_count: u64,
+    data: Vec<u64>,
 }
 
-// TODO: Figure out why I needed mut on combo
-pub fn aggregate_beam_and_splitters(
-    input: &str,
-    mut combo: impl FnMut(u64, u64) -> u64,
-) -> Vec<u64> {
-    let mut splitters = input.split('\n').map(get_row_activations);
-    let initial_beams = splitters.next().expect("Empty sequence");
-    splitters.fold(initial_beams, |mut beams, splitter| {
-        for i in 0..beams.len() {
-            if beams[i] > 0 && splitter[i] > 0 {
-                if i > 0 {
-                    beams[i - 1] = combo(beams[i - 1], beams[i]);
+impl Row {
+    fn parse(s: &str) -> Self {
+        let data = iter::once(0)
+            .chain(s.chars().map(|c| (c != '.') as u64))
+            .chain(iter::once(0))
+            .collect();
+        return Row {
+            split_count: 0,
+            data: data,
+        };
+    }
+
+    fn split_on(&self, splits: &Row) -> Row {
+        let mut new_count = self.split_count;
+        let mut new_data = vec![0; self.data.len()];
+        for i in 1..(self.data.len() - 1) {
+            if self.data[i] > 0 {
+                if splits.data[i] > 0 {
+                    new_count += 1;
+                    new_data[i - 1] += self.data[i];
+                    new_data[i + 1] += self.data[i];
+                } else {
+                    new_data[i] += self.data[i];
                 }
-                if i < beams.len() - 1 {
-                    beams[i + 1] = combo(beams[i + 1], beams[i]);
-                }
-                beams[i] = 0;
             }
         }
-        return beams;
-    })
+        return Row {
+            split_count: new_count,
+            data: new_data,
+        };
+    }
+
+    fn get_path_count(&self) -> u64 {
+        self.data[1..self.data.len() - 1].iter().sum::<u64>()
+    }
+}
+
+fn process_rows(input: &str) -> Row {
+    let mut rows = input.split('\n').map(Row::parse);
+    let beams = rows.next().expect("Empty sequence");
+    return rows.fold(beams, |a, b| a.split_on(&b));
 }
 
 pub fn count_classical_splits(input: &str) -> String {
-    let mut splits = 0;
-    aggregate_beam_and_splitters(input, |_, _| {
-        splits += 1;
-        return 1;
-    });
-    return (splits / 2).to_string();
+    process_rows(input).split_count.to_string()
 }
 
 pub fn count_quantum_splits(input: &str) -> String {
-    let result = aggregate_beam_and_splitters(input, |new, old| new + old);
-    return result.iter().sum::<u64>().to_string();
+    process_rows(input).get_path_count().to_string()
 }
